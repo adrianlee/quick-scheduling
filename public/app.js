a034073f398cab6e60f466fbd83e8bb02cf63879
Adrian Lee <jun.irok@gmail.com>
2015-01-06 23:50:24 -0800

    Added CLNDR.js angular directive

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var App = angular.module('app', ['ngRoute', 'ngAnimate'], function ($compileProvider) {
+var App = angular.module('scheduling', ['ngRoute', 'ngAnimate', 'tien.clndr'], function ($compileProvider) {
   $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|whatsapp):/);
 });
 
@@ -226,75 +226,77 @@ App.service("recentService", function () {
 App.controller("calendarController", function ($scope) {
   console.log("calendarController");
 
-  var cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  var cal_months_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // $('.parent-element').clndr();
 
-  var cal_current_date = new Date(); 
+  // var cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  // var cal_months_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  // var cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  function Calendar(month, year) {
-    this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
-    this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
-    this.html = '';
-  }
+  // var cal_current_date = new Date(); 
 
-  Calendar.prototype.generateHTML = function() {
+  // function Calendar(month, year) {
+  //   this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
+  //   this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
+  //   this.html = '';
+  // }
 
-    // get first day of month
-    var firstDay = new Date(this.year, this.month, 1);
-    var startingDay = firstDay.getDay();
+  // Calendar.prototype.generateHTML = function() {
+
+  //   // get first day of month
+  //   var firstDay = new Date(this.year, this.month, 1);
+  //   var startingDay = firstDay.getDay();
     
-    // find number of days in month
-    var monthLength = cal_days_in_month[this.month];
+  //   // find number of days in month
+  //   var monthLength = cal_days_in_month[this.month];
     
-    // compensate for leap year
-    if (this.month == 1) { // February only!
-      if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
-        monthLength = 29;
-      }
-    }
+  //   // compensate for leap year
+  //   if (this.month == 1) { // February only!
+  //     if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
+  //       monthLength = 29;
+  //     }
+  //   }
     
-    // do the header
-    var monthName = cal_months_labels[this.month]
-    var html = '<table class="calendar-table">';
-    html += '<tr><th colspan="7">';
-    html +=  monthName + "&nbsp;" + this.year;
-    html += '</th></tr>';
-    html += '<tr class="calendar-header">';
-    for(var i = 0; i <= 6; i++ ){
-      html += '<td class="calendar-header-day">';
-      html += cal_days_labels[i];
-      html += '</td>';
-    }
-    html += '</tr><tr>';
+  //   // do the header
+  //   var monthName = cal_months_labels[this.month]
+  //   var html = '<table class="calendar-table">';
+  //   html += '<tr><th colspan="7">';
+  //   html +=  monthName + "&nbsp;" + this.year;
+  //   html += '</th></tr>';
+  //   html += '<tr class="calendar-header">';
+  //   for(var i = 0; i <= 6; i++ ){
+  //     html += '<td class="calendar-header-day">';
+  //     html += cal_days_labels[i];
+  //     html += '</td>';
+  //   }
+  //   html += '</tr><tr>';
 
-    // fill in the days
-    var day = 1;
-    // this loop is for is weeks (rows)
-    for (var i = 0; i < 9; i++) {
-      // this loop is for weekdays (cells)
-      for (var j = 0; j <= 6; j++) { 
-        html += '<td class="calendar-day">';
-        if (day <= monthLength && (i > 0 || j >= startingDay)) {
-          html += day;
-          day++;
-        }
-        html += '</td>';
-      }
-      // stop making rows if we've run out of days
-      if (day > monthLength) {
-        break;
-      } else {
-        html += '</tr><tr>';
-      }
-    }
-    html += '</tr></table>';
+  //   // fill in the days
+  //   var day = 1;
+  //   // this loop is for is weeks (rows)
+  //   for (var i = 0; i < 9; i++) {
+  //     // this loop is for weekdays (cells)
+  //     for (var j = 0; j <= 6; j++) { 
+  //       html += '<td class="calendar-day">';
+  //       if (day <= monthLength && (i > 0 || j >= startingDay)) {
+  //         html += day;
+  //         day++;
+  //       }
+  //       html += '</td>';
+  //     }
+  //     // stop making rows if we've run out of days
+  //     if (day > monthLength) {
+  //       break;
+  //     } else {
+  //       html += '</tr><tr>';
+  //     }
+  //   }
+  //   html += '</tr></table>';
 
-    this.html = html;
-    return html;
-  }
+  //   this.html = html;
+  //   return html;
+  // }
 
-  $scope.calendar = new Calendar();
-  $(".calendar").html($scope.calendar.generateHTML());
-  $(".calendar2").html((new Calendar(1, 2015)).generateHTML());
+  // $scope.calendar = new Calendar();
+  // $(".calendar").html($scope.calendar.generateHTML());
+  // $(".calendar2").html((new Calendar(1, 2015)).generateHTML());
 })
